1da7874f56d730563707df88d996f6e399cdbe95
Felipe Lozano <david.loza010@gmail.com>
2023-08-08 11:35:36 -0400

    finished project

---
+++ src/components/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+        <learning-resource 
+        v-for="resource in resources" 
+        :id="resource.id"
+        :key="resource.id"
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+        ></learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './learning-resources/LearningResource.vue';
+export default{
+    components:{
+        LearningResource
+    },
+    inject: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
